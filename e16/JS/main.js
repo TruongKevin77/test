2c51400119d3a8f07fdc75f79da5723db4cfde09
Kevin Truong <ktruong9@masonlive.gmu.edu>
2019-04-08 15:14:49 -0400

    changes

--- e16/JS/main.js
+++ e16/JS/main.js
@@ -7,4 +7,46 @@ $(document).ready(function(){
     $('#one').mouseover(function(){
         $('#one').toggleClass('darksalmon');
     });
+    
+    $('#two').mouseleave(function(){
+        $('#two').toggleClass('chartreuse');
+    });
+    
+    $('#second').mouseenter(function(){
+        $('#second').toggleClass('darkviolet');
+    });
+    
+    $('#three').mouseover(function(){
+        $('#three').css('background-color','gray');
+    });
+    
+    $('#three').mouseleave(function(){
+        $('#three').css('background-color','darkcyan');
+    });
+    
+    $('#three').click(function(){
+        $('#third').toggleClass('crimson2'); 
+        $('#three').toggleClass('aquamarine');
+    });
+    
+    $('#fourth').click(function(){
+        $('#fourth').toggleClass('aqua2');  
+    });
+    
+    $('#fifth').mouseenter(function(){
+        $('#fifth').toggleClass('fuchsia');  
+        $('#five').toggleClass('darkolivegreen2');
+    });
+    
+    $('#five').click(function(){
+        $('#fifth').toggleClass('shrink');  
+        $('#five').toggleClass('darksalmon');  
+    });
 });
+
+
+
+
+
+
+
